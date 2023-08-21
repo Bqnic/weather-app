73ecee2ba1250d4f87f8d5e108c7fd81e0e3c5ee
Bqnic <123201175+Bqnic@users.noreply.github.com>
2023-08-21 21:12:01 +0200

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # weather-app
-The Odin Project
+
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather APP",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
